01ace10c53a83591f0cadffc271c45466135c94c
Mirat <miratshah27@gmail.com>
2021-08-16 23:17:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/socialapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.socialapp
+
+import android.app.DownloadManager
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.socialapp.daos.PostDao
+import com.example.socialapp.models.Post
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_post.*
+
+class MainActivity : AppCompatActivity(), IPostAdapter {
+
+    private lateinit var adapter: PostAdapter
+    private var postDao: PostDao = PostDao()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener{
+            val intent = Intent(this, CreatePostActivity::class.java)
+            startActivity(intent)
+        }
+
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        val postCollections = postDao.postCollections
+        val query = postCollections.orderBy("createdAt", Query.Direction.DESCENDING)
+        val recyclerViewOptions = FirestoreRecyclerOptions.Builder<Post>().setQuery(query, Post::class.java).build()
+
+        adapter = PostAdapter(recyclerViewOptions, this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        adapter.startListening()
+    }
+
+    override fun onLikeClicked(postId: String) {
+        postDao.updateLike(postId)
+    }
+}
